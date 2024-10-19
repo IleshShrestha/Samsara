11e9befae3725a6aa3b31cb31d2547d026876f4c
Ilesh Shrestha <ilesh.k.shrestha@gmail.com>
2024-10-19 03:56:08 -0400

    changed the summary to the side cart

--- Samsara/settings.py
+++ Samsara/settings.py
@@ -16,11 +16,11 @@ SECRET_KEY = 'django-insecure-_lsr)o4c-kxuqxnxl^6qeznsr76nxs0i)3639&u8fj^#f))yez
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://samsarabound.com','samsarabound.com','samsara-production.up.railway.app', 'https://samsara-production.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://samsara-production.up.railway.app', 'https://samsarabound.com',]
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['https://samsarabound.com','samsarabound.com','samsara-production.up.railway.app', 'https://samsara-production.up.railway.app', '127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['https://samsara-production.up.railway.app', 'https://samsarabound.com',]
+ALLOWED_HOSTS = []
 
 # Application definition
 
