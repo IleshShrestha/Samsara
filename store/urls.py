f1d6eef7dbca0cb78314e98105c92555e8300af8
Ilesh Shrestha <ilesh.k.shrestha@gmail.com>
2024-10-21 00:08:11 -0400

    pushing

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from . import views, settings
-from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,4 +9,4 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('product/<int:pk>', views.product, name='product'),
     path('category/<str:cat>', views.category, name='category'),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] 
