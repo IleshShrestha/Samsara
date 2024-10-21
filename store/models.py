d787d72829a2553b2a6b2b77043250276bc4eb36
Ilesh Shrestha <ilesh.k.shrestha@gmail.com>
2024-10-20 23:16:48 -0400

    got rid of on hover and made the image required

--- store/models.py
+++ store/models.py
@@ -28,8 +28,7 @@ class Product(models.Model):
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', null=True, blank=True)
-    image = models.ImageField(upload_to='uploads/product', null=True, blank=True)
-    image_hover = models.ImageField(upload_to='uploads/hover', null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/product')
     
     # Sales Stuff
     is_sale = models.BooleanField(default=False)
